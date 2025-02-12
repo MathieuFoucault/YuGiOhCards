59cfc409ee73cb12eb9e79fd5d9d3b6d022d3148
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-02-12 10:10:14 +0100

    feat : modif app router from back

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,7 @@ const app = express();
 import cors from "cors";
 
 if (process.env.CLIENT_URL != null) {
-  app.use(cors({ origin: [process.env.CLIENT_URL] }));
+	app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
 }
 
 // If you need to allow extra origins, you can add something like this:
@@ -52,13 +52,13 @@ app.use(
 
 // Uncomment one or more of these options depending on the format of the data sent by your client:
 
-// app.use(express.json());
-// app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded());
 // app.use(express.text());
 // app.use(express.raw());
 
 /* ************************************************************************* */
-
+app.use(cookieParser());
 // Import the API router
 import router from "./router";
 
@@ -83,7 +83,7 @@ import path from "node:path";
 const publicFolderPath = path.join(__dirname, "../../server/public");
 
 if (fs.existsSync(publicFolderPath)) {
-  app.use(express.static(publicFolderPath));
+	app.use(express.static(publicFolderPath));
 }
 
 // Serve client resources
@@ -91,13 +91,13 @@ if (fs.existsSync(publicFolderPath)) {
 const clientBuildPath = path.join(__dirname, "../../client/dist");
 
 if (fs.existsSync(clientBuildPath)) {
-  app.use(express.static(clientBuildPath));
+	app.use(express.static(clientBuildPath));
 
-  // Redirect unhandled requests to the client index file
+	// Redirect unhandled requests to the client index file
 
-  app.get("*", (_, res) => {
-    res.sendFile("index.html", { root: clientBuildPath });
-  });
+	app.get("*", (_, res) => {
+		res.sendFile("index.html", { root: clientBuildPath });
+	});
 }
 
 /* ************************************************************************* */
@@ -106,15 +106,16 @@ if (fs.existsSync(clientBuildPath)) {
 // Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
 
 import type { ErrorRequestHandler } from "express";
+import cookieParser from "cookie-parser";
 
 // Define a middleware function to log errors
 const logErrors: ErrorRequestHandler = (err, req, res, next) => {
-  // Log the error to the console for debugging purposes
-  console.error(err);
-  console.error("on req:", req.method, req.path);
+	// Log the error to the console for debugging purposes
+	console.error(err);
+	console.error("on req:", req.method, req.path);
 
-  // Pass the error to the next middleware in the stack
-  next(err);
+	// Pass the error to the next middleware in the stack
+	next(err);
 };
 
 // Mount the logErrors middleware globally
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import itemActions from "./modules/item/itemActions";
+import { comparePassword, hashPassword } from "./middlewares/argon.middleware";
+import { checkEmail, verifieEmail } from "./middlewares/checkEmail.middleware";
+import { userRegister, adminRegister } from "./middlewares/register.middleware";
+import { checkAdminRole, checkUserRole } from "./middlewares/role.middleware";
+import { login, verifyToken } from "./modules/auth/authActions";
+import userActions from "./modules/item/user/userActions";
 
 const router = express.Router();
 
@@ -6,13 +13,23 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Define item-related routes
-import itemActions from "./modules/item/itemActions";
+router.post(
+	"/api/userformregister",
+	hashPassword,
+	checkEmail,
+	userRegister,
+	userActions.add,
+);
+
+router.post(
+	"/api/login/user",
+	verifieEmail,
+	comparePassword,
+	checkUserRole,
+	login,
+);
 
 router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-
-/* ************************************************************************* */
+// router.use(authenticateToken);
 
 export default router;
